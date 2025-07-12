fd35bd99a9beafd951b0a95bfd82cc0b3c6ccd9d
djeu1116 <rladbgus781@gmail.com>
2025-07-12 14:36:55 +0900

    swagger, security 추가

--- src/main/java/backend/community/global/security/SecurityConfig.java
+++ src/main/java/backend/community/global/security/SecurityConfig.java
@@ -1,29 +1,63 @@
 package backend.community.global.security;
 
+import backend.community.global.auth.jwt.JwtAuthenticationFilter;
+import backend.community.global.auth.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtProvider jwtTokenProvider;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf((csrfConfig) ->
-                        csrfConfig.disable()
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception {
+        return httpSecurity
+                // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                // JWT를 사용하므로 세션을 사용 x
+                // Spring Security가 HTTP 세션을 생성하거나 사용하지 않도록 설정
+                // JWT 기반 인증은 세션을 사용하지 않기 때문에 반드시 STATELESS로 설정
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.
+                                requestMatchers(
+                                        "/auth/**",
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources/**",
+                                        "/webjars/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
                 )
-                .headers((headersConfig) ->
-                        headersConfig.frameOptions(frameOptionsConfig ->
-                                frameOptionsConfig.disable())
+                // UsernamePasswordAuthenticationFilter가 실행되기 전에 JwtAuthenticationFilter를 실행
+                // JWT는 로그인 요청을 거치지 않고도 토큰만으로 인증하므로, Spring 기본 로그인 필터 앞에서 JWT를 먼저 해석해서 인증 상태를 등록
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class
                 )
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests.anyRequest().permitAll()
-                )
-
+                .build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
